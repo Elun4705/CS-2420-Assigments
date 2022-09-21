b03e8c7318d58afcf5da0e28ead85570ddd16c01
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2022-09-21 17:04:54 -0600

    Good Work This Afternoon
    Don't forget later tonight

--- Assign/src/assign04/AnagramChecker.java
+++ Assign/src/assign04/AnagramChecker.java
@@ -1,10 +1,13 @@
 package assign04;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.Scanner;
 
 import assign03.IntegerComparator;
 
@@ -17,15 +20,19 @@ public class AnagramChecker {
 		
 		IntegerComparator intcmp = new IntegerComparator();
 		
-		Character[] stringArray = new Character[] {'v','g','d','b'};
+		String[] stringArray = new String[] {"word", "luna", "luna", "december", "implementgram", "implementgram"};
 		
-		CharacterComparator chrcmp = new CharacterComparator();
+		StringComparator stringcmp = new StringComparator();
 		
 		insertionSort(intArray, intcmp);
 		
-		insertionSort(stringArray, chrcmp);
 		
-		System.out.println(Arrays.toString(stringArray));
+		try {
+			System.out.println(Arrays.toString(getLargestAnagramGroup("sample_word_list.txt")));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		System.out.println(sort("abscsdfasdfasdfa"));
 		
@@ -96,44 +103,51 @@ public class AnagramChecker {
 	//method with a new Comparator class or lambda expression that you design.
 	public static String[] getLargestAnagramGroup(String[] stringArray) {
 		
-		HashMap<String, Integer> anagramSize = new HashMap<String, Integer>(stringArray.length);
-		String bigAna;
+		StringComparator stringcmp = new StringComparator();
+		String[] sortedArray = new String[stringArray.length];
 		
-		for (int i = 0; i < stringArray.length-1; i++) {
-			
-			char[] charArray = stringArray[i].toCharArray();
-			Character[] CharacterArray = new Character[charArray.length];
-			
-			for(int j = 0; j < charArray.length-1; j++) {
-				CharacterArray[j] = Character.valueOf(charArray[j]);
+		for (int i = 0; i < stringArray.length; i++) {
+			String toSort = stringArray[i];
+			sortedArray[i] = sort(toSort);
+		}
+		
+		insertionSort(sortedArray, stringcmp);
+		
+		int highestCount = 0;
+		int currentCount = 0;
+		String anaHigh = null;
+		
+		for (int j = 0; j < sortedArray.length; j++) {
+			String compareWord = sortedArray[j];
+			for(int k = 0; k < sortedArray.length; k++) {
+				if(areAnagrams(compareWord, sortedArray[k]))
+					 currentCount++;
+			}
+			if (currentCount > highestCount) {
+				highestCount = currentCount;
+				anaHigh = sortedArray[j];
 			}
 			
-			CharacterComparator chrcmp = new CharacterComparator();
-			insertionSort(CharacterArray, chrcmp);
-			
-			char[] sortedCharArray = new char[CharacterArray.length];
-			
-			for(int k = 0; k < sortedCharArray.length-1; k++) {
-				sortedCharArray[k] = (CharacterArray[k].charValue());
-			}
-			
-			String lexiOrder = sortedCharArray.toString();
-			
-			if(anagramSize.containsKey(lexiOrder)) {
-				anagramSize.put(lexiOrder, anagramSize.get(lexiOrder) + 1); 
-			} else {
-				anagramSize.put(lexiOrder, 1);
+			currentCount = 0;
+		}
+		
+		String[] emptyArray = new String[0];
+		
+		if (highestCount == 1) {
+			return emptyArray;
+		}
+		
+		String[] highestCountArray = new String[highestCount];
+		int highestCountArrayIndex = 0;
+		
+		for (int l = 0; l < stringArray.length; l++) {
+			if(areAnagrams(anaHigh, stringArray[l])) {
+				highestCountArray[highestCountArrayIndex] = stringArray[l];
+				highestCountArrayIndex++;
 			}
 		}
 		
-		for(int i = 1; i < stringArray.length-1; i++) {
-		    String key = stringArray[i];
-		    if(anagramSize.get(key) > anagramSize.get(stringArray[i-1]))
-		    	bigAna = key;
-		    
-		}
-		
-		return key;
+		return highestCountArray;
 
 	}
 	
@@ -141,17 +155,76 @@ public class AnagramChecker {
 	//It is assumed that the file contains one word per line.  If the file does not exist or is empty, 
 	//the method returns an empty array because there are no anagrams. 
 	// This method must call your getLargestAnagramGroup(String[]) method.
-	public static String[] getLargestAnagramGroup(String filename) {
+	public static String[] getLargestAnagramGroup(String filename) throws FileNotFoundException {
 		
-		String[] obsolete = new String[] {"START", "THIS", "ASAP"};
+		File file = new File(filename);
 		
-		return obsolete;
+		Scanner lengthScanner = new Scanner(file);
+		Scanner fileScanner = new Scanner(file);
+		int arrayLength = 0;
+		
+		while(lengthScanner.hasNextLine()) {
+			arrayLength++;
+			lengthScanner.nextLine();
+		}
+		
+		String[] stringArray = new String[arrayLength];
+		
+		for(int p = 0; p < arrayLength; p++) {
+			stringArray[p] = fileScanner.next();
+		}
+		
+		StringComparator stringcmp = new StringComparator();
+		String[] sortedArray = new String[stringArray.length];
+		
+		for (int i = 0; i < stringArray.length; i++) {
+			String toSort = stringArray[i];
+			sortedArray[i] = sort(toSort);
+		}
+		
+		insertionSort(sortedArray, stringcmp);
+		
+		int highestCount = 0;
+		int currentCount = 0;
+		String anaHigh = null;
+		
+		for (int j = 0; j < sortedArray.length; j++) {
+			String compareWord = sortedArray[j];
+			for(int k = 0; k < sortedArray.length; k++) {
+				if(areAnagrams(compareWord, sortedArray[k]))
+					 currentCount++;
+			}
+			if (currentCount > highestCount) {
+				highestCount = currentCount;
+				anaHigh = sortedArray[j];
+			}
+			
+			currentCount = 0;
+		}
+		
+		String[] emptyArray = new String[0];
+		
+		if (highestCount == 1) {
+			return emptyArray;
+		}
+		
+		String[] highestCountArray = new String[highestCount];
+		int highestCountArrayIndex = 0;
+		
+		for (int l = 0; l < stringArray.length; l++) {
+			if(areAnagrams(anaHigh, stringArray[l])) {
+				highestCountArray[highestCountArrayIndex] = stringArray[l];
+				highestCountArrayIndex++;
+			}
+		}
+		
+		return highestCountArray;
 		
 	}
 	
-	public static class CharacterComparator implements Comparator<Character> {
-		public int compare(Character o1, Character o2) {
-			return Character.compare(o1, o2);
+	public static class StringComparator implements Comparator<String> {
+		public int compare(String o1, String o2) {
+			return o1.compareTo(o2);
 		}
 	}
 
