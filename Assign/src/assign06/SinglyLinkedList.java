84d545500d9de04fcc7fb6c3294337fa909f012b
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2022-10-17 20:33:34 -0600

    puh

--- Assign/src/assign06/SinglyLinkedList.java
+++ Assign/src/assign06/SinglyLinkedList.java
@@ -7,63 +7,107 @@ import java.util.NoSuchElementException;
 import assign03.ArrayCollection;
 
 public class SinglyLinkedList implements List<Object> {
+	
+	private Node head = null;
+	private int size = 0;
+	
+	private class Node {
+		Object element;
+		Node next;
 
-	private ArrayList<Object> arg = new ArrayList<Object>();
+		Node prev;
+
+		Node(Object data) {
+			element = data;
+			next = null;
+			prev = null;
+		}
+
+		
+
+	}
 
 	public SinglyLinkedList() {
 
 	}
+	
+	void add(Object data)
+    {
+ 
+        // Creating new node with given value
+        Node temp = new Node(data);
+ 
+        // Checking if list is empty
+        // and assigning new value to head node
+        if (this.head == null) {
+            head = temp;
+        }
+ 
+        // If list already exists
+        else {
+ 
+            // Temporary node for traversal
+            Node X = head;
+ 
+            // Iterating till end of the List
+            while (X.next != null) {
+                X = X.next;
+            }
+ 
+            // Adding new valued node at the end of the list
+            X.next = temp;
+        }
+ 
+        // Increasing length after adding new node
+        size++;
+    }
 
 	@Override
 	public void insertFirst(Object element) {
-		arg.add(0, element);
-
 	}
 
 	@Override
 	public void insert(int index, Object element) throws IndexOutOfBoundsException {
-		arg.add(index, element);
 
 	}
 
 	@Override
 	public Object getFirst() throws NoSuchElementException {
-		return arg.get(0);
+		return head.element;
 	}
 
 	@Override
 	public Object get(int index) throws IndexOutOfBoundsException {
-		return arg.get(index);
+		Node temp = head;
+		for (int i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+		return temp.element;
 	}
 
 	@Override
 	public Object deleteFirst() throws NoSuchElementException {
-		return arg.remove(0);
 	}
 
 	@Override
 	public Object delete(int index) throws IndexOutOfBoundsException {
-		return arg.remove(index);
 	}
 
 	@Override
 	public int indexOf(Object element) {
-		return arg.indexOf(element);
 	}
 
 	@Override
 	public int size() {
-		return arg.size();
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return arg.isEmpty();
 	}
 
 	@Override
 	public void clear() {
-		arg.clear();
 	}
 
 	@Override
@@ -76,5 +120,5 @@ public class SinglyLinkedList implements List<Object> {
 	public Iterator<Object> iterator() {
 		return null;
 	}
-	
+
 }
