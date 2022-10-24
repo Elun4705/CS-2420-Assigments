16dbfbd44ef5f19efbfb0eab77e14ba48ef41107
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2022-10-23 20:21:01 -0600

    o

--- Assign/src/assign07/BinarySearchTree.java
+++ Assign/src/assign07/BinarySearchTree.java
@@ -4,12 +4,138 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.NoSuchElementException;
 
+import org.junit.jupiter.api.io.TempDir;
+
 public class BinarySearchTree<Type extends Comparable<? super Type>> implements SortedSet<Type> {
+	
+	private BinaryNode<Type> root = null;
+	private Type temp = root.data;
+	
+	@SuppressWarnings("hiding")
+	public class BinaryNode<Type> {
+
+		private Type data;
+
+		private BinaryNode<Type> leftChild;
+
+		private BinaryNode<Type> rightChild;
+
+		public BinaryNode(Type data, BinaryNode<Type> leftChild, BinaryNode<Type> rightChild) {
+			this.data = data;
+			this.leftChild = leftChild;
+			this.rightChild = rightChild;
+		}
+
+		public BinaryNode(Type data) {
+			this(data, null, null);
+		}
+		
+		public Type getData() {
+			return data;
+		}
+
+		/**
+		 * @param data - the node data to be set
+		 */
+		public void setData(Type data) {
+			this.data = data;
+		}
+
+		/**
+		 * @return reference to the left child node
+		 */
+		public BinaryNode<Type> getLeftChild() {
+			return leftChild;
+		}
+
+		/**
+		 * @param leftChild - reference of the left child node to be set
+		 */
+		public void setLeftChild(BinaryNode<Type> leftChild) {
+			this.leftChild = leftChild;
+		}
+
+		/**
+		 * @return reference to the right child node
+		 */
+		public BinaryNode<Type> getRightChild() {
+			return rightChild;
+		}
+
+		/**
+		 * @param rightChild - reference of the right child node to be set
+		 */
+		public void setRightChild(BinaryNode<Type> rightChild) {
+			this.rightChild = rightChild;
+		}
+
+		/**
+		 * @return reference to the leftmost node in the binary tree rooted at this node
+		 */
+		public BinaryNode<Type> getLeftmostNode() {
+			if (leftChild == null)
+				return this;
+			
+			BinaryNode<Type> temp = this;
+			while (temp.getLeftChild() != null)
+				temp = temp.getLeftChild();
+			return temp;
+		}
+
+		/**
+		 * @return reference to the rightmost node in the binary tree rooted at this node
+		 */
+		public BinaryNode<Type> getRightmostNode() {
+			if (rightChild == null)
+				return this;
+			
+			BinaryNode<Type> temp = this;
+			while (temp.getRightChild() != null)
+				temp = temp.getRightChild();
+			return temp;
+			}
+
+		/**
+		 * @return the height of the binary tree rooted at this node
+		 * 
+		 * The height of a tree is the length of the longest path to a leaf
+		 * node. Consider a tree with a single node to have a height of zero.
+		 */
+		public int height() {
+			if (leftChild == null & rightChild == null)
+				return 0;
+			
+			int leftCount = 0;
+			int rightCount = 0;
+			
+			if (leftChild != null)
+				leftCount = leftChild.height();
+			
+
+			if (rightChild != null)
+				rightCount = rightChild.height();
+			
+			return Math.max(leftCount, rightCount) + 1;
+		}
+	}
 
 	@Override
 	public boolean add(Type item) {
-		// TODO Auto-generated method stub
-		return false;
+
+//		if (this.contains(item)) {
+//			return false;
+//		}
+//		
+//		if (item.compareTo(temp) < 0) {
+//			setLeftChild(item);
+//			return true;
+//		}
+//		
+//		if (item.compareTo(temp) > 0) {
+//			this.leftChild = item;
+//			return true;
+//		}
+		
 	}
 
 	@Override
@@ -26,7 +152,21 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 
 	@Override
 	public boolean contains(Type item) {
-		// TODO Auto-generated method stub
+		
+//		if (item.compareTo(this.temp) == 0) {
+//			return true;
+//		}
+//	
+//		if (item.compareTo(temp) < 0) {
+//			this.temp = root.rightChild.getData();
+//			contains(item);
+//		}
+//		
+//		if (item.compareTo(temp) > 0) {
+//			this.temp = root.leftChild.getData();
+//			contains(item);
+//		}
+		
 		return false;
 	}
 
