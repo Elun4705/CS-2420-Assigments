60d73b146435a64948d669ed966c3ba5455a4daa
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2022-11-14 20:57:51 -0700

    ln

---
+++ Assign/src/assign09/HashTable.java
@@ -0,0 +1,87 @@
+package assign09;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HashTable<K, V> implements Map<K, V> {
+	
+	private ArrayList<LinkedList<MapEntry<K, V>>> table;
+	
+	private int capacity;
+	private double lf;
+	
+	public HashTable() {
+		table = new ArrayList<LinkedList<MapEntry<K, V>>>();
+		for(int i = 0; i < capacity; i++)
+		   table.add(new LinkedList<MapEntry<K, V>>());
+	}
+
+	@Override
+	public void clear() {
+		lf = 0;
+		for(LinkedList<MapEntry<K, V>> item : table) {
+			table.remove(item);
+		}
+		
+	}
+
+	@Override
+	public boolean containsKey(K key) {
+		int index = key.hashCode();
+
+		if (table.get(index) != null) {
+			return true;
+		}
+		return false;
+		
+	}
+
+	@Override
+	public boolean containsValue(V value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<MapEntry<K, V>> entries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		if (table.contains(key)) {
+			table
+		}
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public static void main(String args[]) {
+		HashTable test = new HashTable();
+		tes
+	}
+}
