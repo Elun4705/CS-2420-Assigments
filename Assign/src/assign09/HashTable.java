b0f3d54ccc28e153952dc78309bb7c1797924947
ahuo2003 <112907227+ahuo2003@users.noreply.github.com>
2022-11-17 12:17:49 -0700

    po

--- Assign/src/assign09/HashTable.java
+++ Assign/src/assign09/HashTable.java
@@ -5,42 +5,67 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A class for a HashTable using separate chaining.  The table's capacity is initially
+ * set to 1 for ease of testing, however, this can be changed quite easily at any time by simply
+ * changing the instance variable below.
+ * 
+ * @author Andy Huo and Emmanuel Luna
+ *
+ * @param <K> Key
+ * @param <V> Value
+ */
 public class HashTable<K, V> implements Map<K, V> {
-	
+
 	private ArrayList<LinkedList<MapEntry<K, V>>> table;
-	
+
 	private int capacity = 1;
 	private int size = 0;
-	
+
 	public HashTable() {
 		table = new ArrayList<LinkedList<MapEntry<K, V>>>();
-		for(int i = 0; i < capacity; i++)
-		   table.add(new LinkedList<MapEntry<K, V>>());
+		for (int i = 0; i < capacity; i++)
+			table.add(new LinkedList<MapEntry<K, V>>());
 	}
 
+	/**
+	 * Clears every item from the HashTable
+	 */
 	@Override
 	public void clear() {
-		for(LinkedList<MapEntry<K, V>> list : table) {
+		for (LinkedList<MapEntry<K, V>> list : table) {
 			list.clear();
 		}
 		size = 0;
 	}
 
+	/**
+	 * Returns true if the given key is found within the HashTable, false otherwise.
+	 * 
+	 * @param key - the key to be searched for
+	 * @return whether or not the HashTable contains the given key
+	 */
 	@Override
 	public boolean containsKey(K key) {
 		int index = key.hashCode() % capacity;
 
-		LinkedList<MapEntry<K,V>> list = table.get(index);
-		
-		for (MapEntry<K,V> entry : list) {
+		LinkedList<MapEntry<K, V>> list = table.get(index);
+
+		for (MapEntry<K, V> entry : list) {
 			if (entry.getKey() == key) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 
+	/**
+	 * Returns true if the HashTable contains the given value, false otherwise
+	 * 
+	 * @param value - the value to be searched for
+	 * @return whether or not the HashTable contains the given value
+	 */
 	@Override
 	public boolean containsValue(V value) {
 		for (LinkedList<MapEntry<K, V>> list : table) {
@@ -50,78 +75,106 @@ public class HashTable<K, V> implements Map<K, V> {
 				}
 			}
 		}
-		
+
 		return false;
 	}
 
+	/**
+	 * Returns a list of all the entries in the HashTable, in no particular order.
+	 * 
+	 * @return a list of all values in the HashTable
+	 */
 	@Override
 	public List<MapEntry<K, V>> entries() {
-		
+
 		ArrayList<MapEntry<K, V>> entries = new ArrayList<MapEntry<K, V>>();
-		
+
 		for (LinkedList<MapEntry<K, V>> list : table) {
 			entries.addAll(list);
 		}
-		
+
 		return entries;
 	}
 
+	/**
+	 * Returns the value associated with the given key
+	 * 
+	 * @param key - the key to be searched for
+	 * @return the value associated with the given key, or null if there is no such mapping
+	 */
 	@Override
 	public V get(K key) {
 		int index = key.hashCode() % capacity;
 
-		LinkedList<MapEntry<K,V>> list = table.get(index);
-		
+		LinkedList<MapEntry<K, V>> list = table.get(index);
+
 		for (MapEntry<K, V> entry : list) {
 			if (entry.getKey() == key) {
 				return entry.getValue();
 			}
 		}
-		
+
 		return null;
-		
+
 	}
 
+	/**
+	 * Returns whether or not the HashTable is empty
+	 * 
+	 * @return true if the HashTable is empty, false otherwise
+	 */
 	@Override
 	public boolean isEmpty() {
-		return (table.size() == 0);
+		return (size == 0);
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	/**
+	 * Adds the given key and value pair as a mapping to the list
+	 * 
+	 * @param key - the mapping key
+	 * @param value - the mapping value
+	 * @return the prior value associated with that key, or null if there was no such value
+	 */
 	@Override
 	public V put(K key, V value) {
-		
-		double loadFactor = (size+1)/(double) capacity;
-		
+
+		double loadFactor = (size + 1) / (double) capacity;
+
 		if (loadFactor > 10) {
 			this.rehash();
 		}
-		
+
 		int index = key.hashCode() % capacity;
-		
-		LinkedList<MapEntry<K,V>> list = table.get(index);
-		
-		for (MapEntry<K,V> entry : list) {
+
+		LinkedList<MapEntry<K, V>> list = table.get(index);
+
+		for (MapEntry<K, V> entry : list) {
 			if (entry.getKey() == key) {
 				V preValue = entry.getValue();
 				entry.setValue(value);
 				return preValue;
 			}
 		}
-		
-		list.add(new MapEntry(key, value));
+
+		list.add(new MapEntry<K, V>(key, value));
 		size++;
 		return null;
-		
+
 	}
 
+	/**
+	 * Removes the mapping associated with the key and returns it
+	 * 
+	 * @param key - the key to find the removeable value
+	 * @return the value last associated with the key, or null if there is no such value
+	 */
 	@Override
 	public V remove(K key) {
 		int index = key.hashCode() % capacity;
-		
-		LinkedList<MapEntry<K,V>> list = table.get(index);
-		
-		for (MapEntry<K,V> entry : list) {
+
+		LinkedList<MapEntry<K, V>> list = table.get(index);
+
+		for (MapEntry<K, V> entry : list) {
 			if (entry.getKey() == key) {
 				V removeValue = entry.getValue();
 				list.remove(entry);
@@ -129,78 +182,49 @@ public class HashTable<K, V> implements Map<K, V> {
 				return removeValue;
 			}
 		}
-		
+
 		return null;
 	}
 
+	/**
+	 * Returns the number of objects in the HashTable
+	 * 
+	 * @return the number of objects in the HashTable
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
-	
+
+	/**
+	 * A private helper method which will rehash the HashTable in the event the load factor might go over 10
+	 */
 	private void rehash() {
 		List<MapEntry<K, V>> entries = this.entries();
-		
+
 		ArrayList<LinkedList<MapEntry<K, V>>> newTable = new ArrayList<LinkedList<MapEntry<K, V>>>();
-		
+
 		capacity *= 2;
-		
-		for(int i = 0; i < capacity; i++)
-		   newTable.add(new LinkedList<MapEntry<K, V>>());
-		
+
+		for (int i = 0; i < capacity; i++)
+			newTable.add(new LinkedList<MapEntry<K, V>>());
+
 		size = 0;
 		table = newTable;
-		
-		for (MapEntry<K,V> entry : entries) {
+
+		for (MapEntry<K, V> entry : entries) {
 			this.put(entry.getKey(), (entry.getValue()));
 		}
-		
+
 	}
-	
+
+	/**
+	 * A method which returns the current capacity of the HashTable for ease of testing purposes only.
+	 * 
+	 * @return the capacity of the HashTable (or, the size of the backing array)
+	 */
 	public int getCapacity() {
 		return capacity;
 	}
-	
-	
-	public static void main(String args[]) {
-		HashTable test = new HashTable();
-		
-		System.out.println(test.put(1, "worth"));
-		System.out.println(test.put(2, "word"));
-		System.out.println(test.put(3,"banana"));
-		System.out.println(test.put(4,"sleep"));
-		System.out.println(test.put(5,"asian"));
-		System.out.println(test.put(6,"hispanic"));
-		System.out.println(test.put(7, "blossom"));
-		System.out.println(test.put(8, "vega"));
-		System.out.println(test.put(9,"betelgeuse"));
-		System.out.println(test.put(10,"october"));
-		
-		List<MapEntry<Integer, String>> testEntries = test.entries();
 
-		for (MapEntry<Integer, String> entry : testEntries) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
-		}
-		
-		System.out.println(test.size());
-		
-		System.out.println(test.put(11,"watcher"));
-		System.out.println(test.put(12,"warden"));
-		System.out.println(test.put(13,"watcher"));
-		System.out.println(test.put(14,"warden"));
-		System.out.println(test.put(15,"watcher"));
-		System.out.println(test.put(16,"warden"));
-		System.out.println(test.put(17,"watcher"));
-		System.out.println(test.put(18,"warden"));
-		System.out.println(test.put(19,"watcher"));
-		System.out.println(test.put(20,"warden"));
-		System.out.println(test.put(21,"watcher"));
-
-		
-		List<MapEntry<Integer, String>> testEntries2 = test.entries();
-
-		for (MapEntry<Integer, String> entry : testEntries2) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
-		}
-	}
 }
--- Assign/src/assign09/HashTableTest.java
+++ Assign/src/assign09/HashTableTest.java
@@ -2,6 +2,7 @@ package assign09;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -10,53 +11,297 @@ class HashTableTest {
 
 	@Test
 	void testPut() {
-		
+
 		HashTable<Integer, String> test = new HashTable<Integer, String>();
-		
+
 		assertNull(test.put(1, "worth"));
+		assertEquals(1, test.size());
 		assertNull(test.put(2, "word"));
-		assertNull(test.put(3,"banana"));
-		assertNull(test.put(4,"sleep"));
-		assertNull(test.put(5,"asian"));
-		assertNull(test.put(6,"hispanic"));
+		assertEquals(2, test.size());
+		assertNull(test.put(3, "banana"));
+		assertEquals(3, test.size());
+		assertNull(test.put(4, "sleep"));
+		assertEquals(4, test.size());
+		assertNull(test.put(5, "asian"));
+		assertEquals(5, test.size());
+		assertNull(test.put(6, "hispanic"));
+		assertEquals(6, test.size());
 		assertNull(test.put(7, "blossom"));
+		assertEquals(7, test.size());
 		assertNull(test.put(8, "vega"));
-		assertNull(test.put(9,"betelgeuse"));
-		
+		assertEquals(8, test.size());
+		assertNull(test.put(9, "betelgeuse"));
 		assertEquals(9, test.size());
-		
+
 		assertEquals("worth", test.put(1, "night"));
 		assertEquals("night", test.put(1, "walk"));
 		assertEquals("vega", test.put(8, "nyx"));
-		
-		assertNull(test.put(10,"october"));
-		
-		assertEquals(10, test.size());
+
+		assertNull(test.put(10, "october"));
+
 	}
-	
+
 	@Test
 	void testPutRehash() {
 		HashTable<Integer, String> test = new HashTable<Integer, String>();
 
 		assertNull(test.put(1, "worth"));
 		assertNull(test.put(2, "word"));
-		assertNull(test.put(3,"banana"));
-		assertNull(test.put(4,"sleep"));
-		assertNull(test.put(5,"asian"));
-		assertNull(test.put(6,"hispanic"));
+		assertNull(test.put(3, "banana"));
+		assertNull(test.put(4, "sleep"));
+		assertNull(test.put(5, "asian"));
+		assertNull(test.put(6, "hispanic"));
 		assertNull(test.put(7, "blossom"));
 		assertNull(test.put(8, "vega"));
-		assertNull(test.put(9,"betelgeuse"));
-		assertNull(test.put(10,"october"));
-		
+		assertNull(test.put(9, "betelgeuse"));
+		assertNull(test.put(10, "october"));
+
 		assertEquals(10, test.size());
 		assertEquals(1, test.getCapacity());
-		
+
 		assertNull(test.put(11, "water"));
-		
+
 		assertEquals(11, test.size());
 		assertEquals(2, test.getCapacity());
 
-		
+	}
+
+	@Test
+	void testList() {
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		List<MapEntry<Integer, String>> testEntries = test.entries();
+
+		List<MapEntry<Integer, String>> compare = new ArrayList<MapEntry<Integer, String>>();
+
+		compare.add(new MapEntry<Integer, String>(1, "worth"));
+		compare.add(new MapEntry<Integer, String>(2, "word"));
+		compare.add(new MapEntry<Integer, String>(3, "banana"));
+		compare.add(new MapEntry<Integer, String>(4, "sleep"));
+		compare.add(new MapEntry<Integer, String>(5, "asian"));
+		compare.add(new MapEntry<Integer, String>(6, "hispanic"));
+		compare.add(new MapEntry<Integer, String>(7, "blossom"));
+		compare.add(new MapEntry<Integer, String>(8, "vega"));
+		compare.add(new MapEntry<Integer, String>(9, "betelgeuse"));
+		compare.add(new MapEntry<Integer, String>(10, "october"));
+
+		assertEquals(compare, testEntries);
+
+	}
+
+	@Test
+	void testClear() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		assertEquals("worth", test.put(1, "test"));
+
+		assertEquals(10, test.size());
+
+		test.clear();
+
+		assertEquals(0, test.size());
+
+		assertNull(test.put(1, "worth"));
+		assertEquals(1, test.size());
+
+	}
+
+	@Test
+	void testContainsKey() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		assertTrue(test.containsKey(1));
+		assertTrue(test.containsKey(2));
+		assertTrue(test.containsKey(10));
+		assertFalse(test.containsKey(11));
+
+		test.put(11, "wanderlust");
+		assertTrue(test.containsKey(11));
+	}
+
+	@Test
+	void testContainsValue() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		assertTrue(test.containsValue("worth"));
+		assertTrue(test.containsValue("word"));
+		assertTrue(test.containsValue("vega"));
+
+		assertFalse(test.containsValue("whisper"));
+
+		test.put(10, "whisper");
+
+		assertFalse(test.containsValue("october"));
+		assertTrue(test.containsValue("whisper"));
+
+	}
+
+	@Test
+	void testGet() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		assertEquals("worth", test.get(1));
+		assertEquals("banana", test.get(3));
+
+		test.put(1, "bark");
+		assertEquals("bark", test.get(1));
+
+	}
+
+	@Test
+	void testIsEmpty() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		assertTrue(test.isEmpty());
+
+		test.put(1, "worth");
+
+		assertFalse(test.isEmpty());
+
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		test.put(11, "focus");
+
+		test.clear();
+
+		assertTrue(test.isEmpty());
+	}
+
+	@Test
+	void testRemove() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		assertNull(test.remove(11));
+		assertEquals("worth", test.remove(1));
+		assertNull(test.remove(1));
+
+	}
+
+	@Test
+	void testSize() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		assertNull(test.put(1, "worth"));
+		assertEquals(1, test.size());
+		assertNull(test.put(2, "word"));
+		assertEquals(2, test.size());
+		assertNull(test.put(3, "banana"));
+		assertEquals(3, test.size());
+		assertNull(test.put(4, "sleep"));
+		assertEquals(4, test.size());
+		assertNull(test.put(5, "asian"));
+		assertEquals(5, test.size());
+		assertNull(test.put(6, "hispanic"));
+		assertEquals(6, test.size());
+		assertNull(test.put(7, "blossom"));
+		assertEquals(7, test.size());
+		assertNull(test.put(8, "vega"));
+		assertEquals(8, test.size());
+		assertNull(test.put(9, "betelgeuse"));
+		assertEquals(9, test.size());
+	}
+
+	@Test
+	void getCapacity() {
+
+		HashTable<Integer, String> test = new HashTable<Integer, String>();
+
+		test.put(1, "worth");
+		test.put(2, "word");
+		test.put(3, "banana");
+		test.put(4, "sleep");
+		test.put(5, "asian");
+		test.put(6, "hispanic");
+		test.put(7, "blossom");
+		test.put(8, "vega");
+		test.put(9, "betelgeuse");
+		test.put(10, "october");
+
+		assertEquals(1, test.getCapacity());
+
+		test.put(11, "walk");
+
+		assertEquals(2, test.getCapacity());
 	}
 }
