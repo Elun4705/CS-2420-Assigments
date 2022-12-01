a609ffa3255cb1e185b2c8aadbce6494a1182aa6
ahuo2003 <112907227+ahuo2003@users.noreply.github.com>
2022-12-01 16:13:33 -0700

    asd

--- Assign/src/comprehensive/RandomPhraseGenerator.java
+++ Assign/src/comprehensive/RandomPhraseGenerator.java
@@ -1,13 +1,32 @@
 package comprehensive;
 
-public class RandomPhraseGenerator {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		String grammer = args[0];
-		Integer count = Integer.valueOf(args[1]);
+public class RandomPhraseGenerator<E> {
+	
+	HashMap<E,E> map = new HashMap<E,E>();
+
+	public static void main(String[] args) throws FileNotFoundException {
+//		String grammer = args[0];
+//		Integer count = Integer.valueOf(args[1]);
 		
-		System.out.println(grammer);
-		System.out.println(count);
+		scanFile("C:/Users/u1050952/Documents/GitHub/CS-2420-Assigments/Assign/src/comprehensive/poetic_sentence.g");
+//		System.out.println(count);
+	}
+	
+	private static void scanFile(String fileName) throws FileNotFoundException {
+		File file = new File(fileName);
+		Scanner scan = new Scanner(new FileReader(file));
+		while(scan.hasNextLine()) {
+			if (scan.nextLine().contains("{")) {
+				System.out.println("Found!");
+			}
+			scan.nextLine();
+		}
 	}
 	
 }
