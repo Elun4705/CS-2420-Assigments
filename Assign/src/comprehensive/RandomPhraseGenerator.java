9ed2b8d3c1bef4ef0f29c5353089c64073f37638
ahuo2003 <112907227+ahuo2003@users.noreply.github.com>
2022-12-03 03:35:35 -0700

    Check it out!

--- Assign/src/comprehensive/RandomPhraseGenerator.java
+++ Assign/src/comprehensive/RandomPhraseGenerator.java
@@ -3,16 +3,21 @@ package comprehensive;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class RandomPhraseGenerator<E> {
 
-	HashMap<E, E> map = new HashMap<E, E>();
+	private static String StartPoint = null;
+	private static HashMap<String, ArrayList<String>> groups = new HashMap<String, ArrayList<String>>();
+	private static int currCount = 1;
+	private static boolean open = false;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		String grammer = args[0];
-		Integer count = Integer.valueOf(args[1]);
+//		String grammer = args[0];
+//		Integer count = Integer.valueOf(args[1]);
 
 // cd Documents\GitHub\CS-2420-Assigments\Assign\src
 // java comprehensive/RandomPhraseGenerator
@@ -21,28 +26,44 @@ public class RandomPhraseGenerator<E> {
 // C:/Users/u1050952/Documents/GitHub/CS-2420-Assigments/Assign/src/comprehensive/poetic_sentence.g 5
 // cd Users\EMoon\Documents\GitHub\CS-2420-Assigments\Assign\src
 
-		for (int i = 0; i < count; i++) {
-			scanFile(grammer);
-		}
+		scanFile("C:/Users/u1050952/Documents/GitHub/CS-2420-Assigments/Assign/src/comprehensive/poetic_sentence.g");
 	}
 
 	private static void scanFile(String fileName) throws FileNotFoundException {
 		File file = new File(fileName);
 		Scanner scan = new Scanner(new FileReader(file));
 
-		while (scan.hasNextLine()) {
-			if (scan.findInLine("<start>") != null)
-				System.out.println("Found start!");
-			if (scan.findInLine("<object>") != null)
-				System.out.println("Found object!");
-			if (scan.findInLine("<verb>") != null)
-				System.out.println("Found verb!");
-			if (scan.findInLine("<adverb>") != null)
-				System.out.println("Found adverb!");
-			scan.nextLine();
-		}
 		
-		System.out.println();
+		// From: Andy, To: Emmanuel
+		// Please don't touch this, I worked hard on it.  It's three in the morning as I'm typing this.
+		// For easier access, I changed our HashMap to a HashMap of ArrayLists.  Why this is important will be shown
+		// soon.
+		// Basically, what this will do is when it finds an open bracket, it sets the next line as "cat" (short for
+		// category.)  It will then set the life after cat to "check."  "Cat" will become the Key in our HashMap.
+		// Then it runs a while loop, adding each example of the category to the ArrayList at the appropriate key.
+		// The examples are kept track of by "check," which is updated through every iteration of the while loop, and
+		// will continue to do so as long as "check" doesn't become the closed bracket.  Once check finds a closed
+		// bracket, it will go back out to the original while loop, which looks for more open brackets.
+		while (scan.hasNextLine()) {
+			if (scan.nextLine().compareTo("{") == 0) {
+				String cat = scan.nextLine();
+				cat = cat.substring(1, cat.length() - 1);
+				String check = scan.nextLine();
+				while (check.compareTo("}") != 0) {
+					if(groups.containsKey(cat) == false) {
+						ArrayList<String> arr = new ArrayList<String>();
+						arr.add(check);
+						groups.put(cat, arr);
+					} else {
+						groups.get(cat).add(check);
+					}
+					check = scan.nextLine();
+				}
+			}
+		}
+
+		// This is what I used to check out the inside of my HashMap.
+		System.out.println(Arrays.asList(groups));
 
 	}
 
